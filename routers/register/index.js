1a5953db95c546f97b509475f70ef172fe5d9e99
wzk <1356422517@qq.com>
2022-11-10 17:25:34 +0800

    昵称正则

--- routers/register/index.js
+++ routers/register/index.js
@@ -4,7 +4,7 @@ module.exports = async ctx => {
   console.log("注册账号密码：:", ctx.request.body);
   const { user = '', password = '', name } = ctx.request.body;
 
-  if (!/[a-zA-Z0-9_]{1,16}[\u4e00-\u9fa5]{1,8}/.test(name)) {
+  if (!/([a-zA-Z0-9_]{1,16})|([\u4e00-\u9fa5]{2,8})|([a-zA-Z0-9_\u4e00-\u9fa5]{3,12})/.test(name)) {
     return ctx.body = {
       code: 100,
       message: "昵称超出规定范围"
