430ac23dc3572dd4b6dd1846061d156d660007b2
wzk <1356422517@qq.com>
2022-11-10 18:04:49 +0800

    默认值

--- routers/register/index.js
+++ routers/register/index.js
@@ -2,7 +2,7 @@ const userTable = require("../../mongodb/user");
 
 module.exports = async ctx => {
   console.log("注册账号密码：:", ctx.request.body);
-  const { user = '', password = '', name } = ctx.request.body;
+  const { user = '', password = '', name = '' } = ctx.request.body;
 
   if (!/([a-zA-Z0-9_]{1,16})|([\u4e00-\u9fa5]{2,8})|([a-zA-Z0-9_\u4e00-\u9fa5]{3,12})/.test(name)) {
     return ctx.body = {
